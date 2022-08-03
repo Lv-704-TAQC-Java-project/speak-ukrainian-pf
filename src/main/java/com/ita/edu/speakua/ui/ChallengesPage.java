bbe25998f5136b5e9f6676124208422a146317d2
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-08-03 17:56:33 +0300

    add AboutPage, ChallengesPage

---
+++ src/main/java/com/ita/edu/speakua/ui/ChallengesPage.java
@@ -0,0 +1,20 @@
+package com.ita.edu.speakua.ui;
+
+import org.openqa.selenium.WebDriver;
+
+public class ChallengesPage extends BasePage {
+
+    // private HeaderComponent header;
+
+    public ChallengesPage(WebDriver driver) {
+        super(driver);
+    }
+
+    //    public HeaderComponent getHeader() {
+//        if (header == null) {
+//            header = new HeaderComponent(driver);
+//        }
+//        return header;
+//    }
+
+}
