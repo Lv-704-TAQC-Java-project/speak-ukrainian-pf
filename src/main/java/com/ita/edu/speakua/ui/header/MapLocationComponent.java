29784996690973f1d90ee614ee72365e03c8b3e7
DankoMaksym <dankomaksym@gmail.com>
2022-08-03 19:53:13 +0300

    dankomax - create HeaderComponent

---
+++ src/main/java/com/ita/edu/speakua/ui/header/MapLocationComponent.java
@@ -0,0 +1,25 @@
+package com.ita.edu.speakua.ui.header;
+
+import com.ita.edu.speakua.ui.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+
+
+public class MapLocationComponent extends BasePage {
+    @FindBy(xpath = "//button[contains(@class,'show-map-button')]")
+    private WebElement showOnMapButton;
+
+    @FindBy(xpath = "//input[@id='mapCitiesList']/ancestor::div[@class='ant-select-selector']")
+    private WebElement locationMenuButton;
+
+
+    public MapLocationComponent(WebDriver driver) {
+        super(driver);
+    }
+
+
+    public MapLocationComponent showMap() {
+        showOnMapButton.click();
+        return this;
+    }
+}
---
+++ src/main/java/com/ita/edu/speakua/ui/header/PopupMessageComponent.java
@@ -0,0 +1,15 @@
+package com.ita.edu.speakua.ui.header;
+
+import com.ita.edu.speakua.ui.BasePage;
+import org.openqa.selenium.WebDriver;
+
+
+public class PopupMessageComponent extends BasePage {
+
+
+    public PopupMessageComponent(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
