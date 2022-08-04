6394c31aa25f1b888f24569acc278717e5291b6f
DankoMaksym <dankomaksym@gmail.com>
2022-08-04 13:44:56 +0300

    dankomax - move runners and utils to ui

--- src/test/java/com/ita/edu/speakua/ui/runners/BaseTestRunner.java
+++ src/test/java/com/ita/edu/speakua/ui/runners/BaseTestRunner.java
@@ -1,5 +1,6 @@
-package runners;
+package com.ita.edu.speakua.ui.runners;
 
+import com.ita.edu.speakua.ui.utils.ConfigProperties;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import utils.ConfigProperties;
 
 import java.time.Duration;
 
--- src/test/java/com/ita/edu/speakua/ui/utils/ConfigProperties.java
+++ src/test/java/com/ita/edu/speakua/ui/utils/ConfigProperties.java
@@ -1,4 +1,4 @@
-package utils;
+package com.ita.edu.speakua.ui.utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
