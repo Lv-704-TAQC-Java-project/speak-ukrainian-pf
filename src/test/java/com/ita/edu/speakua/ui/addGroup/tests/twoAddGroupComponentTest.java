82fd9e5a4db90aacfd95db5b2ef4955e5987b9ed
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-07 18:56:54 +0300

    ykireyeva: added empty twoAddGroupComponentTest

---
+++ src/test/java/com/ita/edu/speakua/ui/addGroup/tests/twoAddGroupComponentTest.java
@@ -0,0 +1,12 @@
+package com.ita.edu.speakua.ui.addGroup.tests;
+
+import com.ita.edu.speakua.ui.runners.BaseTestRunnerWithLogIn;
+import org.testng.annotations.Test;
+
+public class twoAddGroupComponentTest extends BaseTestRunnerWithLogIn {
+
+    @Test
+    public void verifyDescribeComponentWithValidData(){
+    }
+
+}
