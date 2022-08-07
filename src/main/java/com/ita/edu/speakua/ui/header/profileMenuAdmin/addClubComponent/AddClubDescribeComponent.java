77b8b6eefa33442f4531732a338047d88b2ac7ec
DankoMaksym <dankomaksym@gmail.com>
2022-08-08 01:22:59 +0300

    dankomax - add verifyLengthErrorMessageForDescriptionFieldWhenAddClub Test

--- src/main/java/com/ita/edu/speakua/ui/header/profileMenuAdmin/addClubComponent/AddClubDescribeComponent.java
+++ src/main/java/com/ita/edu/speakua/ui/header/profileMenuAdmin/addClubComponent/AddClubDescribeComponent.java
@@ -1,11 +1,13 @@
 package com.ita.edu.speakua.ui.header.profileMenuAdmin.addClubComponent;
 
 import com.ita.edu.speakua.ui.HomePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.time.Duration;
+import java.util.List;
 
 public class AddClubDescribeComponent extends AbstractAddClubComponent {
 
@@ -17,6 +19,9 @@ public class AddClubDescribeComponent extends AbstractAddClubComponent {
 
     @FindBy(xpath = "//span[contains(text(),'Завершити')]/ancestor::button")
     WebElement finishBtn;
+
+    @FindBy(xpath = "//div[contains(@class, 'explain-error')]")
+    private List<WebElement> errorMessagesForDescriptionField;
 
     @FindBy(xpath = "//div[contains(@class, 'explain')]//div[1]")
     private WebElement errorMessageDescriptionFieldPart1;
@@ -60,5 +65,16 @@ public class AddClubDescribeComponent extends AbstractAddClubComponent {
         return this;
     }
 
-
+    public boolean lengthErrorMessageForDescriptionFieldIsPresent(String errorMsg) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        if (errorMessagesForDescriptionField != null && errorMessagesForDescriptionField.size() > 0) {
+            for (WebElement error: errorMessagesForDescriptionField) {
+                waitVisibilityOfWebElement(error);
+                if (error.getText().equals(errorMsg)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
