1bef6270a9202b4f54104d81c741670c92fdc283
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-07 19:34:10 +0300

    ykireyeva: added twoAddGroupComponentTest implementation

--- src/test/java/com/ita/edu/speakua/ui/addGroup/tests/twoAddGroupComponentTest.java
+++ src/test/java/com/ita/edu/speakua/ui/addGroup/tests/twoAddGroupComponentTest.java
@@ -1,12 +1,42 @@
 package com.ita.edu.speakua.ui.addGroup.tests;
 
+import com.ita.edu.speakua.ui.HomePage;
+import com.ita.edu.speakua.ui.header.profileMenuAdmin.addClubComponent.AddClubDescribeComponent;
 import com.ita.edu.speakua.ui.runners.BaseTestRunnerWithLogIn;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class twoAddGroupComponentTest extends BaseTestRunnerWithLogIn {
 
     @Test
     public void verifyDescribeComponentWithValidData(){
-    }
+        boolean btnFinishIsEnable;
+        String[] testData = new String[]{"Education, students, Школа балету, Teachers",
+                "1234567890123456789012345678901234567890",
+                "!\"#$%&'()*+,-./:;<=>?@[]^_`{}~"};
 
+        AddClubDescribeComponent addClubDescribeComponent = new HomePage(driver)
+                .openAdminProfileMenu()
+                .openUserProfilePage()
+                .openAddClubModal()
+                .inputNameOfClub("Club3")
+                .chooseCategoryClub("Основи")
+                .inputAgeFrom(4)
+                .inputAgeTo(9)
+                .clickNextStep()
+                .inputPhoneNumber("0973756135")
+                .clickNextStep();
+
+        SoftAssert softAssert = new SoftAssert();
+
+        for (String data : testData){
+            addClubDescribeComponent
+                    .inputDescribe(data);
+
+            btnFinishIsEnable = addClubDescribeComponent.finishBtnIsEnable();
+            softAssert.assertTrue(btnFinishIsEnable);
+        }
+
+        softAssert.assertAll();
+    }
 }
