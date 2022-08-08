36067af16eccc04c04e87904a8607aee01769174
Jaroslav Tarasovych <staryk90@gmail.com>
2022-08-08 19:57:58 +0300

    Yaroslav: create new Runner BaseTestRunnerAddTaskDescribe!

---
+++ src/test/java/com/ita/edu/speakua/ui/runners/BaseTestRunnerAddTaskDescribe.java
@@ -0,0 +1,25 @@
+package com.ita.edu.speakua.ui.runners;
+
+import com.ita.edu.speakua.ui.HomePage;
+import com.ita.edu.speakua.ui.header.profileMenuAdmin.addClubComponent.AddClubDescribeComponent;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTestRunnerAddTaskDescribe extends BaseTestRunnerWithLogIn{
+
+    protected AddClubDescribeComponent addClubDescribeComponent;
+
+    @BeforeClass
+    public void openAddClubDescribeComponent(){
+        addClubDescribeComponent = new HomePage(driver)
+                .openAdminProfileMenu()
+                .openUserProfilePage()
+                .openAddClubModal()
+                .inputNameOfClub("Спортивні танці")
+                .chooseCategoryClub("Спортивні секції")
+                .inputAgeFrom(4)
+                .inputAgeTo(8)
+                .clickNextStep()
+                .inputPhoneNumber("0672131246")
+                .clickNextStep();
+    }
+}
