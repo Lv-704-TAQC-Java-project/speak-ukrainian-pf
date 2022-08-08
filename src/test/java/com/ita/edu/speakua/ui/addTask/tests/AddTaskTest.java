8600d748e0ee4ff7b39a87846fc1dba005d6bbe1
Olenka <olenkadashinich@gmail.com>
2022-08-08 22:20:50 +0300

    added tests on location and advanced search panel

--- src/test/java/com/ita/edu/speakua/ui/addTask/tests/AddTaskTest.java
+++ src/test/java/com/ita/edu/speakua/ui/addTask/tests/AddTaskTest.java
@@ -93,4 +93,33 @@ public class AddTaskTest extends BaseTestRunnerWithLogIn {
         softAssert.assertTrue(errorMessageHeadingTooManyChars, "A message should appear stating that too many " +
                 "characters have been entered");
     }
+
+
+
+    @Test
+    public void verifyCreateTaskWithoutChallenge(){
+        AddTaskPage addTaskPage = new HomePage(driver)
+                .openAdminProfileMenu()
+                .openAdministrationModal()
+                .clickTaskBtn()
+                .clickAddTaskBtn();
+
+
+        addTaskPage = addTaskPage
+                .inputStartDate("2023-01-01")
+                .inputName("Lorem Ipsum")
+                .inputHeading("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has " +
+                        "been the industry's standard dummy text ever since the 1500s, when an unknown printer took a " +
+                        "galley of type and scrambled it to make a type specimen book. It has survived not only five " +
+                        "centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+                        "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum " +
+                        "passages, and more recently with desktop publishing software like Aldus PageMaker including " +
+                        "versions of Lorem Ipsum")
+                .inputDescribing("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has " +
+                        "been the industry's standard dummy text ever since the 1500s, when an unknown printer took a ")
+                .tryClickSaveButton();
+
+
+        Assert.assertTrue(addTaskPage.errorMessageIsEmptyIsVisible(), "Error message didn't find");
+    }
 }
