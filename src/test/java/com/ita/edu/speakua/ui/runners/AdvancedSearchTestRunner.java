bc0b5f2786641df83ee73122b5c59082f8754dff
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-11 16:49:12 +0300

    ykireyeva: added AdvancedSearchPanelComponent initialization to AdvancedSearchTestRunner

--- src/test/java/com/ita/edu/speakua/ui/runners/AdvancedSearchTestRunner.java
+++ src/test/java/com/ita/edu/speakua/ui/runners/AdvancedSearchTestRunner.java
@@ -1,15 +1,18 @@
 package com.ita.edu.speakua.ui.runners;
 
+import com.ita.edu.speakua.ui.clubs.AdvancedSearchPanelComponent;
 import com.ita.edu.speakua.ui.header.HeaderComponent;
 import org.testng.annotations.BeforeClass;
 
 public class AdvancedSearchTestRunner extends SameWindowTestRunner {
+    protected AdvancedSearchPanelComponent advancedSearchPanel;
 
     @BeforeClass
     public void openAdvancedSearch() {
-        new HeaderComponent(driver)
+        advancedSearchPanel = new HeaderComponent(driver)
                 .getNavigationComponent()
                 .clickClubsBtn()
-                .advancedSearchButtonClick();
+                .advancedSearchButtonClick()
+                .getAdvancedSearchPanelComponent();
     }
 }
