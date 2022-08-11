0bc4ce559c7d1202cf9c3390f875443b92ca1136
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-08-11 23:55:44 +0300

    added steps, description, issueNumber for addLocationTest

--- src/test/java/com/ita/edu/speakua/ui/addCenter/tests/AddLocationTest.java
+++ src/test/java/com/ita/edu/speakua/ui/addCenter/tests/AddLocationTest.java
@@ -3,6 +3,8 @@ package com.ita.edu.speakua.ui.addCenter.tests;
 import com.ita.edu.speakua.ui.header.HeaderComponent;
 import com.ita.edu.speakua.ui.runners.LoginTestRunner;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -12,14 +14,16 @@ import java.util.Date;
 
 public class AddLocationTest extends LoginTestRunner {
 
-    @DataProvider(name = "cityFromDropDownAddLocationMenu")
+    @DataProvider(name = "menuDataAddLocation")
     public Object[][] cityFromDropDownAddLocationMenu() {
         return new Object[][]{
                 {"Київ", "Деснянський", "вул. Садова, 1а", "50.4485253, 30.4735083", "0632233456"}
         };
     }
 
-    @Test(dataProvider = "cityFromDropDownAddLocationMenu")
+    @Issue("TUA-158")
+    @Description("Verify that admin can add new location to the locations list")
+    @Test(dataProvider = "menuDataAddLocation")
     public void addLocationTest(String city, String region, String address, String coordinates, String phoneNumber) {
         String name = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
         HeaderComponent header = new HeaderComponent(driver);
