1de3563c6d3084d31320e398b8e7f44f491ed342
DankoMaksym <dankomaksym@gmail.com>
2022-09-09 10:44:12 +0300

    dankomax - fixed import in AddTaskTestRunner

--- src/test/java/com/ita/edu/speakua/ui/runners/AddTaskTestRunner.java
+++ src/test/java/com/ita/edu/speakua/ui/runners/AddTaskTestRunner.java
@@ -1,7 +1,7 @@
 package com.ita.edu.speakua.ui.runners;
 
 import com.ita.edu.speakua.ui.header.profileMenuAdmin.administrationMenu.addTask.AddTaskPage;
-import com.ita.edu.speakua.ui.utils.jdbc.services.TaskService;
+import com.ita.edu.speakua.utils.jdbc.services.TaskService;
 import org.testng.annotations.BeforeMethod;
 
 public class AddTaskTestRunner extends LoginTestRunner {
